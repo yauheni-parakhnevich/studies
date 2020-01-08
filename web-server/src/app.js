167ea69be3eeae4791b9ef992c075859de77e4cc
yparakhnevich <yauheni_parakhnevich@epam.com>
2020-01-08 17:45:40 +0100

    Templates added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,43 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directories to serve
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Yauheni'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Yauheni'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        message: 'Some text message',
+        name: 'Yauheni'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
