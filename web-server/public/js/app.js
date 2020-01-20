74922804369d92a0a57443d352bbdd77c8f6fa46
yparakhnevich <yauheni_parakhnevich@epam.com>
2020-01-20 16:49:43 +0100

    Linking api to UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,28 @@
 console.log('CLient-side javascript file is loaded')
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log('Error: ' + data.error)
-        } else {
-            console.log(data.location, data.forecast);
-        }
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = 'Error: ' + data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
