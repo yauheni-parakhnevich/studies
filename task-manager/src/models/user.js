e2ca19964816bf5b41e104a87e6a5e2353d74029
Yauheni Parakhnevich <yauheniparakhnevich@Yauhenis-MacBook-Pro-9.local>
2020-02-18 23:40:30 +0100

    Separate storage for routes, passord hashed, login added

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -23,6 +25,7 @@ const User = mongoose.model('User', {
         required: true,
         trim: true,
         lowercase: true ,
+        unique: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
@@ -37,7 +40,52 @@ const User = mongoose.model('User', {
                 throw new Error('Age must be positive')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString()}, 'thisisasecretkey')
+
+    user.tokens = user.tokens.concat({ token })
+    user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email})
+
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plaintext password
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
